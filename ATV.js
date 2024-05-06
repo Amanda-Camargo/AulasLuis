0b3c965d4a20b073a9dd56bd2c4db86e632e53ea
Amanda Camargo <160730194+Amanda-Camargo@users.noreply.github.com>
2024-05-06 12:03:49 -0300

    Atividades Javascript - Lógica da programação.

---
+++ ATV.js
@@ -0,0 +1,20 @@
+/* let nome = prompt('Qual é o seu nome?');
+alert('Seu nome é ' + nome + '.'); 
+
+let idade = prompt("Qual é sua idade?");
+alert('Sua idade é ' + idade + '.');
+ */
+
+/* let nome = prompt('Qual é o seu nome?');
+alert('Seu nome é ' + nome + '.'); */
+
+/* let idade = prompt('Qual é sua idade?');
+alert('Sua idade é ' + idade + '.'); */
+
+let idade = prompt('Digite a sua idade');
+if (idade > 18) {
+    alert('Menores de idade não podem tirar a habilitação.')
+}
+else{
+    alert('Você pode tirar a habilitação.')
+}
